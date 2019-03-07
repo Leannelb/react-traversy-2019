3aa1b2461d56fdd29aa40673200a270eaa37b3ca
Leanne Lacey <leanne.lacey@justsomecoding.com>
2019-03-07 21:18:03 +0100

    display via map

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 class Todos extends Component {
   render() {
     console.log(this.props.todos);
-    return this.props.todos.map((todo));
+    return this.props.todos.map(todo => (
+      <h3>{todo.title}</h3>
+      // todo object. Dot notation to acess property
+    ));
     // just like a forEach
-    );
   }
 }
 
