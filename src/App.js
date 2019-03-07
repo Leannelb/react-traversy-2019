e844b7a56db3d3a35fe694aa124b4d3709071506
Leanne Lacey <leanne.lacey@justsomecoding.com>
2019-03-07 20:49:55 +0100

    create component and import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Todos from "./components/Todos";
 
 import "./App.css";
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi</h1>
+        <Todos />
       </div>
     );
   }
