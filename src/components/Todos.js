e02fecdbed2bd72b3fe0d385662f2704bde9cf9d
Leanne Lacey <leanne.lacey@justsomecoding.com>
2019-03-07 21:16:29 +0100

    todo object. Dot notation to acess property

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,10 +3,8 @@ import React, { Component } from "react";
 class Todos extends Component {
   render() {
     console.log(this.props.todos);
-    return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
+    return this.props.todos.map((todo));
+    // just like a forEach
     );
   }
 }
