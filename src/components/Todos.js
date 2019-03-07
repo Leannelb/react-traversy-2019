266d65501b7e44d98c983a682b374143c2d7c219
Leanne Lacey <leanne.lacey@justsomecoding.com>
2019-03-07 21:40:03 +0100

    giving key gets rid of error message

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,7 +5,7 @@ class Todos extends Component {
   render() {
     console.log(this.props.todos);
     return this.props.todos.map(todo => (
-      <TodoItem todo={todo} />
+      <TodoItem key={todo.id} todo={todo} />
       // todo object. Dot notation to acess property
     ));
     // just like a forEach
